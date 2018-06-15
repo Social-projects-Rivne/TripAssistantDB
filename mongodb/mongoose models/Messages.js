1fe1103954f9a4b9de71803a5d93710cdc12ff81
kibernetika <kibernetika@mail.ua>
2018-06-15 12:07:45 +0300

    added Chats and Message scripts and mongoose models
    Took 6 minutes

---
+++ mongodb/mongoose models/Messages.js
@@ -0,0 +1,17 @@
+{
+    "_id": {
+        "type": "Schema.Types.ObjectId"
+    },
+    "chat": {
+        "type": "Schema.Types.ObjectId"
+    },
+    "create_at": {
+        "type": "Date"
+    },
+    "update_at": {
+        "type": "Date"
+    },
+    "user_id": {
+        "type": "Number"
+    }
+}
