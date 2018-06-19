cdb80b52960f522f1fbacb5a7b633c4a9bcaa220
Stan0men <stan0men@yahoo.com>
2018-06-19 12:03:48 +0300

    added data to table passenger_feedback

---
+++ postgressql/data/passenger_feedbacks.sql
@@ -0,0 +1,26 @@
+--'new','declined_by_moderator','approved_by_moderator','published'
+
+
+
+INSERT INTO passenger_feedbacks (driver_id,passenger_id, route_id,rating,status,feedback)
+VALUES 
+(13,1,1,5,'new', 'Great driver!' ),
+(12,2,2,5,'approved_by_moderator','The driver was drunk! I liked everything!'),
+(3,3,3,2,'approved_by_moderator','The driver  listened chanson'),
+(4,4,4,2,'declined_by_moderator','Odmen petuh'),  
+(5,5,5,5,'new','Declined to take my mother in law!'),
+(6,6,6,1,'approved_by_moderator','Bad hygiene at driver!'),
+(7,7,7,1,'published','Smoked in the car like a train!'),
+(8,8,8,2,'declined_by_moderator','Moder petuh'),  
+(9,9,9,1,'published','Offered money for  love!!!'),
+(10,10,10,5,'approved_by_moderator','The driver was drunk!'),
+(11,11,11,5,'new', 'Great driver!' ),
+(12,12,12,5,'approved_by_moderator','The driver was drunk! I liked everything!'),
+(13,13,13,2,'approved_by_moderator','The driver  listened chanson'),
+(14,14,14,2,'declined_by_moderator','Odmen petuh'),  
+(15,15,15,5,'new','Declined to take my mother in law!'),
+(16,16,16,1,'approved_by_moderator','Bad hygiene at driver!'),
+(17,17,17,1,'published','Smoked in the car like a train!'),
+(18,18,18,2,'declined_by_moderator','Moder petuh'),  
+(19,19,19,1,'published','Offered money for  love!!!'),
+(20,20,20,5,'approved_by_moderator','The driver was drunk!');
